df7c2d4fd8eb086babd30d8aaa3d6aec6315129b
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-03-11 13:45:35 +0100

    new task interaction added

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import "./style.css";
 
 const Form = ({ addNewTask }) => {
-  const [newTaskContent, setNewTaskContent] = useState(" ");
+  const [newTaskContent, setNewTaskContent] = useState("");
   const onFormSubmit = (event) => {
     event.preventDefault();
-    addNewTask(newTaskContent);
+    addNewTask(newTaskContent.trim());
+    setNewTaskContent("");
   };
 
 
@@ -14,8 +15,9 @@ const Form = ({ addNewTask }) => {
       <input
         value={newTaskContent}
         className="form__input"
-        placeholder="Wpisz nowe zadanie" 
-        onChange={(target) => setNewTaskContent(target.value)} />
+        placeholder="Wpisz nowe zadanie"
+        onChange={({ target }) => setNewTaskContent(target.value)}
+      />
       <button className="form__button">Dodaj zadanie</button>
     </form>
   );

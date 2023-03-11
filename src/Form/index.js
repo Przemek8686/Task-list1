a8e3d6600cf4a07872de468c0ff860b1f87b4c54
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-03-11 12:55:14 +0100

    new task interaction not correct yet

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,23 @@
+import React, { useState } from "react";
 import "./style.css";
-const Form = () => (
-    <form className="form">
-          <input className="form__input" placeholder="Wpisz nowe zadanie" autofocus />
-          <button className="form__button">Dodaj zadanie</button>
-        </form>
-);
+
+const Form = ({ addNewTask }) => {
+  const [newTaskContent, setNewTaskContent] = useState(" ");
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    addNewTask(newTaskContent);
+  };
+
+
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <input
+        value={newTaskContent}
+        className="form__input"
+        placeholder="Wpisz nowe zadanie" 
+        onChange={(target) => setNewTaskContent(target.value)} />
+      <button className="form__button">Dodaj zadanie</button>
+    </form>
+  );
+};
 export default Form;

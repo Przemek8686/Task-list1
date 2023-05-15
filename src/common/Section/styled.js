6350542eb634ac5117a0f404cf28f2ebfcbc7f06
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-05-15 19:34:07 +0200

    fotmat code

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -3,28 +3,27 @@ import styled from "styled-components";
 export const Wrapper = styled.section`
   background: ${({ theme }) => theme.color.white};
   margin: 10px 0px;
-  box-shadow:0 0 5px ${({ theme }) => theme.color.alto};
+  box-shadow: 0 0 5px ${({ theme }) => theme.color.alto};
 `;
 export const Header = styled.header`
- border-bottom: 1px solid ${({ theme }) => theme.color.alto};
-    display: grid;
-    grid-template-columns: auto auto;
-    grid-gap: 20px;
-    justify-content: space-between;
-    align-items: center;
-    padding: 20px;
+  border-bottom: 1px solid ${({ theme }) => theme.color.alto};
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 20px;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
 
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        grid-template-columns: 1fr;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
-
 export const Title = styled.h2`
- font-size: 20px;
-    margin: 0%;
+  font-size: 20px;
+  margin: 0%;
 `;
 
 export const SectionBody = styled.div`
-    padding: 20px;
+  padding: 20px;
 `;
--- src/store.js
+++ src/store.js
@@ -2,8 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "./features/tasksSlice";
 
 export default configureStore({
-    reducer:{
-        tasks:tasksReducer,
-
-    },
+  reducer: {
+    tasks: tasksReducer,
+  },
 });

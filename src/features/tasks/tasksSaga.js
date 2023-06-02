694253d31dc4e5f6beab1cc8bdf90043912d2432
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-06-02 20:08:05 +0200

    rename tasks js serch added correction code

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,24 +1,25 @@
-import { takeLatest, takeEvery, call, put, select} from "redux-saga/effects";
-import { fetchExampleTasks, selectTasks, fetchExampleTasksSuccess, fetchExampleTasksError } from "../tasksSlice";
+import { takeLatest, call, put, takeEvery, select, delay } from "redux-saga/effects";
+import { fetchExampleTasks, selectTasks, fetchExampleTasksSuccess, fetchExampleTasksError } from "./tasksSlice";
 import { getExampleTasks } from "./getExampleTasks";
 import { saveTasksInLocalStorage } from "./tasksLocalStorage";
 
 function* fetchExampleTasksHandler() {
-  try {
-    const exampleTasks = yield call(getExampleTasks);
-    yield put(fetchExampleTasksSuccess(exampleTasks));
-  } catch (error) {
-    yield put(fetchExampleTasksError());
-    yield call(alert, "Cos poszło nie tak😥");
-  }
+    try {
+        yield delay(1000);
+        const exampleTasks = yield call(getExampleTasks);
+        yield put(fetchExampleTasksSuccess(exampleTasks));
+    } catch (error) {
+        yield put(fetchExampleTasksError());
+        yield call(alert, "Cos poszlo nie tak!");
+    }
 }
 
 function* saveTasksInLocalStorageHandler() {
-  const tasks = yield select(selectTasks);
-  yield call(saveTasksInLocalStorage, tasks);
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
 }
+
 export function* tasksSaga() {
-  yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
-  yield takeEvery("*", saveTasksInLocalStorageHandler);
-  
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
 }
--- src/features/tasksSlice.js
+++ src/features/tasksSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getTasksFromLocalStorage } from "./tasks/tasksLocalStorage";
+import { getTasksFromLocalStorage } from "./tasksLocalStorage";
+
 const tasksSlice = createSlice({
-  name: `tasks`,
+  name: "tasks",
   initialState: {
     tasks: getTasksFromLocalStorage(),
     hideDone: false,
@@ -10,7 +11,7 @@ const tasksSlice = createSlice({
     addTask: ({ tasks }, { payload: task }) => {
       tasks.push(task);
     },
-    toggleHideDone: state => {
+    toggleHideDone: (state) => {
       state.hideDone = !state.hideDone;
     },
     toggleTaskDone: ({ tasks }, { payload: taskId }) => {
@@ -26,19 +27,19 @@ const tasksSlice = createSlice({
         task.done = true;
       }
     },
-    fetchExampleTasks: state => { 
-      state.loading=true
-    
-},
-fetchExampleTasksSuccess:(state, {payload:tasks})=>{
-  state.tasks=tasks;
-  state.loading=false;
-},
-fetchExampleTasksError:(state) =>{
-  state.loading=false;
-},
-},
+    fetchExampleTasks: (state) => {
+      state.loading = true;
+    },
+    fetchExampleTasksSuccess: (state, { payload: tasks }) => {
+      state.tasks = tasks;
+      state.loading = false;
+    },
+    fetchExampleTasksError: (state) => {
+      state.loading = false;
+    },
+  },
 });
+
 export const {
   addTask,
   toggleHideDone,
@@ -48,15 +49,28 @@ export const {
   fetchExampleTasks,
   fetchExampleTasksSuccess,
   fetchExampleTasksError,
-  setTasks
- } = tasksSlice.actions;
+  setTasks,
+} = tasksSlice.actions;
 
-export const selectTasksState = state => state.tasks;
-export const selectTasks = state => selectTasksState(state).tasks;
-export const selectHideDone = state => selectTasksState(state).hideDone;
-export const selectAreTasksEmpty = state => selectTasks(state).length === 0;
-export const selectIsEveryTaskDone = state =>
+export const selectTasksState = (state) => state.tasks;
+export const selectTasks = (state) => selectTasksState(state).tasks;
+export const selectHideDone = (state) => selectTasksState(state).hideDone;
+export const selectAreTasksEmpty = (state) => selectTasks(state).length === 0;
+export const selectIsEveryTaskDone = (state) =>
   selectTasks(state).every(({ done }) => done);
-  export const selectLoading =state=>selectTasksState(state).loading;
+export const selectLoading = (state) => selectTasksState(state).loading;
 
+export const getTaskById = (state, taskId) =>
+  selectTasks(state).find(({ id }) => id === taskId);
+
+export const selectTasksByQuery = (state, query) => {
+  const tasks = selectTasks(state);
+
+  if (!query || query.trim() === "") {
+    return tasks;
+  }
+  return tasks.filter(({ content }) =>
+    content.toUpperCase().includes(query.trim().toUpperCase())
+  );
+};
 export default tasksSlice.reducer;

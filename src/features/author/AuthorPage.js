7a879faf25b7ed4336a63e78c0ff3cd7f9004747
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-06-03 18:52:55 +0200

    correction image demo and example tasks

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -2,20 +2,39 @@ import Container from "../../common/Container/styled";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
 
-const AuthorPage = () => (
-    <Container>
-        <Header title={"O autorze"} />
-        <Section
-            title="Marzena Malka"
-            body={
-                <>
-                <p>Czesc, nazywam sie Przemek Krawczynski.Od ponad 10 lat mieszkam i pracuje w Holandii. </p> 
-                <p>Dotychczas krztalcilem sie w wielu kierunkach, obecnie uczeszczam na kurs online dotyczacy programowania</p>
-                <p>Interesuje sie <b>sportem,polityka, czytaniem ksiazek oraz muzyka</b></p>
-                </>
-            }
-            />
-    </Container>
+const AuthorPage = () => ( <
+    Container >
+    <
+    Header title = { "O autorze" }
+    />{" "} <
+    Section title = "Przemek Krawczynski"
+    body = { <
+        >
+        <
+        p > { " " }
+        Czesc,
+        nazywam sie Przemek Krawczynski.Od ponad 10 lat mieszkam i
+        pracuje w Holandii. { " " } <
+        /p>{" "} <
+        p > { " " }
+        Ukonczylem szkole w kierunku Technik Ekonomista oraz Technik Ochrony
+        Osob i Mienia,
+        ale przez ostatnie lata ukonczylem rowniez kursy
+        spawania,
+        operatora koparki i wozkow widlowych oraz instruktora
+        pilki noznej.Obcnie uczeszczam na kurs online dotyczacy
+        programowania. { " " } <
+        /p>{" "} <
+        p > { " " }
+        Interesuje sie { " " } <
+        b > sportem,
+        polityka,
+        czytaniem ksiazek oraz muzyka. < /b>{" "} <
+        /p>{" "} <
+        />
+    }
+    />{" "} <
+    /Container>
 );
 
 export default AuthorPage;

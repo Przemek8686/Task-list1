a0c7031f16cf92960dbabc4a75c5db604f6fdd02
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-06-03 13:38:34 +0200

    task page added store correction

--- src/store.js
+++ src/store.js
@@ -1,15 +1,18 @@
+
 import { configureStore } from "@reduxjs/toolkit";
-import  createSagaMiddleware  from "redux-saga";
-import tasksReducer from "./features/tasksSlice";
+import createSagaMiddleware from "redux-saga";
+import tasksReducer from './features/tasks/tasksSlice';
 import rootSaga from "./features/tasks/rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
+
 const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
-  },
-  middleware: [sagaMiddleware],
+    reducer: {
+        tasks: tasksReducer,
+    },
+    middleware: [sagaMiddleware],
 });
 
 sagaMiddleware.run(rootSaga);
+
 export default store;
